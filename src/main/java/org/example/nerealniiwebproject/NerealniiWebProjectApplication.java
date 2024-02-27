b7a6aed3d4fe7dc094aeba88776eb09e6cd98aff
stefan-zayats <stepanzayac@gmail.com>
2024-02-27 12:45:28 +0300

    created new spring project

---
+++ src/main/java/org/example/nerealniiwebproject/NerealniiWebProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.nerealniiwebproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NerealniiWebProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NerealniiWebProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/nerealniiwebproject/NerealniiWebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.nerealniiwebproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NerealniiWebProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
